5c4b7ff7ee9e2db28f6b5bf0e68a5f3bd212d8f0
Britton <brittonkdeets@gmail.com>
2019-08-18 20:23:57 -0400

    back

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
 from macross import MACrossPaper
 import executor
-import os
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "We're up and runnin"
-
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
 
 macross_params = {
     'period': 20,
@@ -21,5 +11,6 @@ strategies = [
     MACrossPaper(macross_params)
 ]
 
+print('y tho')
 for strategy in strategies:
     executor.beginTrading(strategy)
