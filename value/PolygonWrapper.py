ba2ab8326e56cd943ba9cb510fa25eb84eed17cc
Britton <brittonkdeets@gmail.com>
2019-10-09 21:32:41 -0400

    value logic

--- value/PolygonWrapper.py
+++ value/PolygonWrapper.py
@@ -5,7 +5,7 @@ import json
 POLYGON_BASE_URL = os.getenv('POLYGON_BASE_URL')
 KEY = os.getenv('APCA_API_KEY_ID')
 
-def get_fundamentals(ticker, limit=1):
+def get_fundamentals(ticker, limit=2):
     endpoint = 'reference/financials/' + ticker
     url = POLYGON_BASE_URL + endpoint
     response = requests.get(
