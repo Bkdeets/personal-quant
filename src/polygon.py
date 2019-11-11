1b1b97cf9647f6416fea604c9429f95927c14ba5
Britton <brittonkdeets@gmail.com>
2019-11-10 21:12:13 -0500

    just needed api key

--- src/polygon.py
+++ src/polygon.py
@@ -53,10 +53,7 @@ def get_dividends(ticker, limit=7):
         'apiKey': KEY,
         'limit': str(limit)
     }
-    if limit > 0:
-        return make_request(endpoint, ticker, params).get('results')[::-1]
-    else:
-        return None
+    return make_request(endpoint, ticker, params).get('results')[::-1]
 
 def get_candle_by_date(ticker, date, limit=1):
     endpoint = 'v1/open-close/' + ticker + '/' + date
