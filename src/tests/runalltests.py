efe806741a190c60a419872740a72fc5da08d14a
Britton <brittonkdeets@gmail.com>
2019-11-10 21:32:08 -0500

    moar tests

--- src/tests/runalltests.py
+++ src/tests/runalltests.py
@@ -1,6 +1,6 @@
 import os
 from colorama import Fore, Back, Style 
 
-test_command = "/Library/Frameworks/Python.framework/Versions/3.7/bin/python3 -m pytest -rA --tb=long src/tests"
+test_command = "/Library/Frameworks/Python.framework/Versions/3.7/bin/python3 -m pytest -rA --tb=long --cov=src src/tests "
 os.system(test_command)
 print(Fore.GREEN + 'Test suite finished')
