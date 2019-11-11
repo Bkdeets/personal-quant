de98930ece902da489094ca7d41a365eb45388bb
Britton <brittonkdeets@gmail.com>
2019-11-11 10:59:41 -0500

    value funcs testing

--- src/tests/test_value_funcs.py
+++ src/tests/test_value_funcs.py
@@ -1,4 +1,45 @@
 from ..utility import value_funcs as vf
 class TestValueFuncs:
-    def test_imports_work(self):
-        assert True
+    
+    # need polygon mock to test
+    def test_calc_dd_model(self):
+        fundamentals = [{
+            'ticker': 'AAPL'
+        }]
+        result = vf.calc_dd_model(fundamentals)
+        assert not result
+    
+    # need polygon mock to test
+    def test_calc_fcf_model(self):
+        fundamentals = [{
+            'ticker': 'AAPL'
+        }]
+        result = vf.calc_fcf_model(fundamentals)
+        assert not result
+    
+    # need polygon mock to test
+    def test_calc_gd_model(self):
+        fundamentals = [{
+            'ticker': 'AAPL',
+            'earningsPerBasicShareUSD': 100
+        }]
+        result = vf.calc_gd_model(fundamentals)
+        assert not result
+    
+    # need polygon mock to test
+    def test_calc_ri_model(self):
+        fundamentals = [{
+            'ticker': 'AAPL'
+        }]
+        result = vf.calc_ri_model(fundamentals)
+        assert not result
+    
+    # need polygon mock to test
+    def test_calc_models(self):
+        fundamentals = [{
+            'ticker': 'AAPL',
+            'earningsPerBasicShareUSD': 100
+        }]
+        result = vf.calc_models(fundamentals)
+        assert not result
+
