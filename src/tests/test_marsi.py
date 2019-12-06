72bb83ac44ee4375cbe2bd1df06e76c70ff53e7d
Britton <brittonkdeets@gmail.com>
2019-12-06 12:16:36 -0500

    paper patch

--- src/tests/test_marsi.py
+++ src/tests/test_marsi.py
@@ -1,5 +1,7 @@
 from ..strategies.marsi import Marsi
 import datetime
+import pandas as pd 
+
 class MockObj(object):
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
@@ -17,7 +19,11 @@ class MockApi:
     def get_clock(self):
         return True
     def get_account(self):
-        return True
+        return MockObj(
+            cash=1000,
+            equity=1000,
+            buying_power=2000
+        )
     def get_activities(self, activity_types='default'):
         return True
 class TestMarsi:
@@ -28,3 +34,50 @@ class TestMarsi:
     def test_initializes(self):
         self.beforeEach()
         assert self.m.params.get('default')
+
+    def test_getSmaIndication(self):
+        self.beforeEach()
+        self.m.params['sma'] = {'level': .01}
+        current_price = 100
+        current_sma = 120
+        ticker = 'YEEEEETED'
+        result = self.m.getSmaIndication(current_price, current_sma, ticker)
+        assert result == 'short'
+    
+    def test_getRsiIndication(self):
+        self.beforeEach()
+        self.m.params['rsi'] = {'topLevel': 65, 'bottomLevel': 35}
+        current_price = 100
+        current_rsi = 70
+        ticker = 'YEEEEETED'
+        result = self.m.getRsiIndication(current_price, current_rsi, ticker)
+        assert result == 'short'
+
+    def test_getShares(self):
+        self.beforeEach()
+        price = 1000
+        position_size = 1
+        result = self.m.getShares(price, position_size)
+        assert result == 2
+
+    def test_getIndicationOrder(self):
+        self.beforeEach()
+        smaIndication = 'buy'
+        rsiIndication = 'buy'
+        side = 'long'
+        position_size = 1
+        ticker = 'YAHEEEEHEHEHEEET'
+        current_price = 10
+        result = self.m.getIndicationOrder(smaIndication, rsiIndication, side, position_size, ticker, current_price)
+        assert result.get('ticker') == ticker
+    
+    # def test_get_orders(self):
+    #     self.beforeEach()
+    #     prices_df = {'BREEEET': [1,2,3,4]}
+  
+    #     # initialize list of lists 
+    #     data = [['close', 10], ['close', 15], ['close', 14]] 
+        
+    #     # Create the pandas DataFrame 
+    #     df = pd.DataFrame(data, columns = ['Name', 'Age'])
+
--- src/utility/utility.py
+++ src/utility/utility.py
@@ -187,4 +187,3 @@ class Utility:
 
         return barset.df
 
-
