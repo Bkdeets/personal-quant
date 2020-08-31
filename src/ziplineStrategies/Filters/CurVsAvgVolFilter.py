1430746f6202bb3d74b7701ac86e65e09d1e09ab
Britton <brittonkdeets@gmail.com>
2020-08-31 11:13:48 -0400

    some more imports

--- src/ziplineStrategies/Filters/CurVsAvgVolFilter.py
+++ src/ziplineStrategies/Filters/CurVsAvgVolFilter.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 from zipline.pipeline import CustomFilter
-from zipline.pipeline.data import EquityPricing
+from pipeline_live.data.alpaca.pricing import USEquityPricing
 
 def curVsAvgVolFilter(lookback):
     class CurVsAvgVolFilter(CustomFilter):
-        inputs = [EquityPricing.close, EquityPricing.volume]
+        inputs = [USEquityPricing.close, USEquityPricing.volume]
         def compute(self, today, assets, out, close_price, volume):
             avg_dollar_volume = np.mean(close_price * volume, axis=0)
             current_dollar_volume = close_price[-1] * volume[-1]
