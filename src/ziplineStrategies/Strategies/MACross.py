7c45b3c62a12705a1017ed0c8413f08dbcddfaf7
Britton <brittonkdeets@gmail.com>
2020-09-01 21:27:36 -0400

    stop losses and fixing pipeline

--- src/ziplineStrategies/Strategies/MACross.py
+++ src/ziplineStrategies/Strategies/MACross.py
@@ -9,7 +9,6 @@ from pipeline_live.data.iex.factors import (AverageDollarVolume, SimpleMovingAve
 from pylivetrader.api import(
     symbol,
     pipeline_output,
-    attach_pipeline,
     order_target_percent)
 import logging
 
@@ -17,7 +16,6 @@ import logging
 ENGINE = LivePipelineEngine(list_symbols)
 
 def initialize(context):
-
     context.params = {
         'lookback': 20,
         'smaSlowLookback': 30,
@@ -26,11 +24,7 @@ def initialize(context):
     context.position_size = .1
     context.stopLevel = .9
     context.leverageLimit = 1.5
-
-    attach_pipeline(
-        make_pipeline(context), 
-        name='smaCrossPipeline'
-    )
+    context.stopPriceMap = {}
 
 def make_pipeline(context):
     advFilter = curVsAvgVolFilter(context.params.get('lookback'))
@@ -54,20 +48,36 @@ def handle_data(context, data):
     numOfPositions = len(context.portfolio.positions)
     pr = ENGINE.run_pipeline(make_pipeline(context))
     filtered = applyPipelineFilters(pr)
+    entryAndExitLogic(filtered)
+    manageStops(context, data)
+
+def entryAndExitLogic(context, filtered):
     if not filtered.empty:
         for asset, value in filtered.iterrows():
             asset = symbol(asset)
-            currentPrice = data.current(asset, "close")
+            currentPrice = data.current(asset, 'close')
             stopPrice = currentPrice * context.stopLevel
             if longConditionsMet(value, currentPrice):
                 # enter position with position size as long as leverage is below certain level
                 if withinLeverageLimit(context, numOfPositions):
                     logging.info(f'Ordering shares of {asset} at {currentPrice} with a stop at {stopPrice}')
-                    order_target_percent(asset, context.position_size, stop_price=stopPrice)
+                    context.stopPriceMap[asset] = stopPrice
+                    order_target_percent(asset, context.position_size)
                     numOfPositions += 1
             elif asset in context.portfolio.positions.keys():
                 logging.info(f'Exiting position in {asset}')
                 order_target_percent(asset, 0.0)
+
+def manageStops(context, data):
+    for key, value in context.portfolio.positions.items():
+        stopPrice = context.stopPriceMap[asset]
+        currentPrice = data.current(key, 'close')
+        newStop = currentPrice * context.stopLevel
+        if currentPrice < stopPrice:
+            logging.info(f'Stopped out of {asset}')
+            order_target_percent(asset, 0.0)
+        elif newStop > stopPrice:
+            context.stopPriceMap[asset] = newStop
 
 def withinLeverageLimit(context, numOfPositions):
     return (numOfPositions/(context.position_size * 100)) < context.leverageLimit
